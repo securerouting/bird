032affc2c70633175369f81acc075d7e4f73783f
Wes Hardaker <opensource@hardakers.net>
2011-12-16 10:52:17 -0800

    tests to verify signature verification

--- proto/bgp/bgpsec/tests.c
+++ proto/bgp/bgpsec/tests.c
@@ -22,6 +22,9 @@ int main(int argc, char **argv) {
 
     /* test whether we can sign a block of text */
     int algorithm_count = 0;
+
+    int ret;
+    
     while(signature_algorithms[algorithm_count] > 0) {
 
         /* generate a signature using a certificate */
@@ -30,11 +33,21 @@ int main(int argc, char **argv) {
                                        signature_algorithms[algorithm_count],
                                        signature, sizeof(signature));
 
-        RESULT(("cert sign: algorithm %d, signature length (%d) is not negative", signature_algorithms[algorithm_count], signature_len), signature_len > -1);
+        RESULT(("cert sign: algorithm %d, signature length (%d) is not negative",
+                signature_algorithms[algorithm_count], signature_len),
+               signature_len > -1);
         RESULT(("cert sign: algorithm %d, signature length (%d) has at least a byte", signature_algorithms[algorithm_count], signature_len), signature_len > 0);
         
 
-        /* XXX: check the signature */
+        /* verify that the signature matches */
+        ret = bgpsec_verify_signature_with_cert(data_to_sign,
+                                                sizeof(data_to_sign),
+                                                cert,
+                                                signature_algorithms[algorithm_count],
+                                                signature, sizeof(signature));
+        RESULT(("cert sign: verify signature result: %d (should be %d)",
+                ret, BGPSEC_SIGNATURE_MATCH),
+               ret == BGPSEC_SIGNATURE_MATCH);
 
         /* generate a signature using a fingerprint */
         /* XXX: set test directory to search for matching fp->certs */
@@ -43,9 +56,21 @@ int main(int argc, char **argv) {
                                      signature_algorithms[algorithm_count],
                                      signature, sizeof(signature));
 
-        RESULT(("fp sign:   algorithm %d, signature length (%d) is not negative", signature_algorithms[algorithm_count], signature_len), signature_len > -1);
+        RESULT(("fp sign:   algorithm %d, signature length (%d) is not negative",
+                signature_algorithms[algorithm_count], signature_len),
+               signature_len > -1);
         RESULT(("fp sign:   algorithm %d, signature length (%d) has at least a byte", signature_algorithms[algorithm_count], signature_len), signature_len > 0);
         
+
+        /* verify that the signature matches */
+        ret = bgpsec_verify_signature_with_fp(data_to_sign,
+                                              sizeof(data_to_sign),
+                                              fp,
+                                              signature_algorithms[algorithm_count],
+                                              signature, sizeof(signature));
+        RESULT(("fp sign:   verify signature result: %d (should be %d)",
+                ret, BGPSEC_SIGNATURE_MATCH),
+               ret == BGPSEC_SIGNATURE_MATCH);
 
         /* move on to the next algorithm */
         algorithm_count++;
