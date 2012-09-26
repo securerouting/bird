c5dc2bfc3b0aba452c6c3eb8b9b61fcbdfe71adf
Wes Hardaker <opensource@hardakers.net>
2012-09-26 16:55:19 -0700

    Rewrite the tests to test support with and without binary keys

--- proto/bgp/bgpsec/tests.c
+++ proto/bgp/bgpsec/tests.c
@@ -1,13 +1,16 @@
 #include "validate.h"
 #include <sys/param.h>
 
-#define RESULT(test, is_success) {                                      \
-        printf("%7.7s: ", ((is_success) ? "ok" : "not ok"));            \
-        printf("%4d: ", __LINE__);                                      \
-        printf test;                                                    \
-        printf("\n");                                                   \
-        if (is_success) good++;                                         \
-        else bad++;                                                     \
+#define HEADER(msg)       printf("--------------- " msg "\n");
+#define HEADER1(msg, arg) printf("--------------- " msg "\n", arg);
+
+#define RESULT(test, is_success) {                              \
+        printf("%7.7s: ", ((is_success) ? "ok" : "not ok"));    \
+        printf("%4d: ", __LINE__);                              \
+        printf test;                                            \
+        printf("\n");                                           \
+        if (is_success) good++;                                 \
+        else bad++;                                             \
     }
 
 #define DIE(msg) do { fprintf(stderr, "CRITICAL FAIL: %s\n", msg); exit(1); } while(1);
@@ -23,6 +26,7 @@ int main(int argc, char **argv) {
     bgpsec_key_data key_data;
     char ski[1024];
     char filePrefix[MAXPATHLEN];
+    char fileName[MAXPATHLEN];
     int  signature_algorithms[] = { BGPSEC_ALGORITHM_SHA256_ECDSA_P_256, -1 };
     byte data_to_sign[] = { 1,2,3,4,5,6,7,8 };
 
@@ -40,152 +44,196 @@ int main(int argc, char **argv) {
     int good = 0, bad = 0;
 
     /* test whether we can sign a block of text */
-    int algorithm_count = 0;
-
     int ret;
     int curveId;
+    int bin_save;
 
-    /* create a dummy certificate to use */
-    system("../proto/bgp/bgpsec/gen-router-key -d " TEST_KEY_REPO_PATH " -c ../proto/bgp/bgpsec/router-key.cnf -p > ski.txt");
-    fp = fopen("ski.txt", "r");
-    if (NULL == fp)
-        DIE("failed to open the ski.txt file that should have been created");
-    ski[sizeof(ski)-1] = '\0';
-    if (NULL == fgets(ski, sizeof(ski)-1, fp))
-        DIE("Couldn't read the SKI from the ski.txt file");
-    ski[strlen(ski)-1] = '\0'; /* chomp the LF off */
-    fclose(fp);
+    for(bin_save = 0; bin_save < 2; bin_save++) {
+        HEADER1("starting test with binary_keys = %d", bin_save);
 
-    /* now define all the file names based on it the new cert */
-    filePrefix[sizeof(filePrefix)-1] = '\0';
-    snprintf(filePrefix, sizeof(filePrefix)-1, "%s/%s", TEST_KEY_REPO_PATH, ski);
-    generate_ski_filename(filePrefix, sizeof(filePrefix), TEST_KEY_REPO_PATH,
-                          ski, strlen(ski));
-
-    while(signature_algorithms[algorithm_count] > 0) {
-        bgpsec_key_data key_data;
-        curveId = signature_algorithms[algorithm_count];
-
-        ret = bgpsec_load_key(&bgpconfig, filePrefix, &key_data,
-                              curveId, 1);
-        RESULT(("cert sign: loaded the router key from tmp file: %s",
-                filePrefix),
-               ret == BGPSEC_SUCCESS);
+        int algorithm_count = 0;
+        bgpconfig.bgpsec_save_binary_keys = bin_save;
         
 
-        /* generate a signature using a certificate */
-        signature_len =
-            bgpsec_sign_data_with_cert(&bgpconfig,
-                                       data_to_sign, sizeof(data_to_sign),
-                                       key_data,
-                                       signature_algorithms[algorithm_count],
-                                       signature, sizeof(signature));
+        /* create a dummy certificate to use */
+        system("../proto/bgp/bgpsec/gen-router-key -d " TEST_KEY_REPO_PATH " -c ../proto/bgp/bgpsec/router-key.cnf -p > ski.txt");
+        fp = fopen("ski.txt", "r");
+        if (NULL == fp)
+            DIE("failed to open the ski.txt file that should have been created");
+        ski[sizeof(ski)-1] = '\0';
+        if (NULL == fgets(ski, sizeof(ski)-1, fp))
+            DIE("Couldn't read the SKI from the ski.txt file");
+        ski[strlen(ski)-1] = '\0'; /* chomp the LF off */
+        fclose(fp);
 
-        RESULT(("cert sign: algorithm %d, signature length (%d) is not negative",
-                signature_algorithms[algorithm_count], signature_len),
-               signature_len > -1);
-        RESULT(("cert sign: algorithm %d, signature length (%d) has at least a byte", signature_algorithms[algorithm_count], signature_len), signature_len > 0);
+        /* now define all the file names based on it the new cert */
+        filePrefix[sizeof(filePrefix)-1] = '\0';
+        snprintf(filePrefix, sizeof(filePrefix)-1, "%s/%s", TEST_KEY_REPO_PATH, ski);
+        generate_ski_filename(filePrefix, sizeof(filePrefix), TEST_KEY_REPO_PATH,
+                              ski, strlen(ski));
 
-        /* modify the private key so it can't be part of the verification */
-        BN_init(&newbignum);
-        EC_KEY_set_private_key(key_data.ecdsa_key, &newbignum);
+        while(signature_algorithms[algorithm_count] > 0) {
+            HEADER1("-- starting test with sig alg = %d",
+                    signature_algorithms[algorithm_count]);
+            bgpsec_key_data key_data;
+            curveId = signature_algorithms[algorithm_count];
 
-        /* verify that the signature matches */
-        ret = bgpsec_verify_signature_with_cert(&bgpconfig, data_to_sign,
-                                                sizeof(data_to_sign),
-                                                key_data,
-                                                signature_algorithms[algorithm_count],
-                                                signature, signature_len);
-        RESULT(("cert sign: verify signature result: %d (should be %d)",
-                ret, BGPSEC_SIGNATURE_MATCH),
-               ret == BGPSEC_SIGNATURE_MATCH);
+            ret = bgpsec_load_key(&bgpconfig, filePrefix, &key_data,
+                                  curveId, 1);
+            RESULT(("cert sign: loaded the router key from tmp file: %s",
+                    filePrefix),
+                   ret == BGPSEC_SUCCESS);
+        
 
-        /* modify the public key so it can't be part of the verification */
-        /* (which should make the verification fail now) */
-        new_point = EC_POINT_new(EC_GROUP_new_by_curve_name(curveId));
-        EC_KEY_set_public_key(key_data.ecdsa_key, new_point);
-        EC_POINT_free(new_point);
+            /* generate a signature using a certificate */
+            signature_len =
+                bgpsec_sign_data_with_cert(&bgpconfig,
+                                           data_to_sign, sizeof(data_to_sign),
+                                           key_data,
+                                           signature_algorithms[algorithm_count],
+                                           signature, sizeof(signature));
 
-        /* verify that the signature no longer matches */
-        ret = bgpsec_verify_signature_with_cert(&bgpconfig,
+            RESULT(("cert sign: algorithm %d, signature length (%d) is not negative",
+                    signature_algorithms[algorithm_count], signature_len),
+                   signature_len > -1);
+            RESULT(("cert sign: algorithm %d, signature length (%d) has at least a byte", signature_algorithms[algorithm_count], signature_len), signature_len > 0);
+
+            /* modify the private key so it can't be part of the verification */
+            BN_init(&newbignum);
+            EC_KEY_set_private_key(key_data.ecdsa_key, &newbignum);
+
+            /* verify that the signature matches */
+            ret = bgpsec_verify_signature_with_cert(&bgpconfig, data_to_sign,
+                                                    sizeof(data_to_sign),
+                                                    key_data,
+                                                    signature_algorithms[algorithm_count],
+                                                    signature, signature_len);
+            RESULT(("cert sign: verify signature result: %d (should be %d)",
+                    ret, BGPSEC_SIGNATURE_MATCH),
+                   ret == BGPSEC_SIGNATURE_MATCH);
+
+            /* verify that the signature matches */
+            ret = bgpsec_verify_signature_with_cert(&bgpconfig, data_to_sign,
+                                                    sizeof(data_to_sign),
+                                                    key_data,
+                                                    signature_algorithms[algorithm_count],
+                                                    signature, signature_len);
+            RESULT(("cert sign: verify signature result2: %d (should be %d)",
+                    ret, BGPSEC_SIGNATURE_MATCH),
+                   ret == BGPSEC_SIGNATURE_MATCH);
+            
+            /* modify the public key so it can't be part of the verification */
+            /* (which should make the verification fail now) */
+            new_point = EC_POINT_new(EC_GROUP_new_by_curve_name(curveId));
+            EC_KEY_set_public_key(key_data.ecdsa_key, new_point);
+            EC_POINT_free(new_point);
+
+            /* verify that the signature no longer matches */
+            ret = bgpsec_verify_signature_with_cert(&bgpconfig,
+                                                    data_to_sign,
+                                                    sizeof(data_to_sign),
+                                                    key_data,
+                                                    signature_algorithms[algorithm_count],
+                                                    signature, signature_len);
+            RESULT(("cert sign: verify signature fail result: %d (should be %d)",
+                    ret, BGPSEC_SIGNATURE_MISMATCH),
+                   ret == BGPSEC_SIGNATURE_MISMATCH);
+        
+            /* completely get rid of the current key */
+            EC_KEY_free(key_data.ecdsa_key);
+            key_data.ecdsa_key = NULL;
+
+            /* now reload the key from the files and use them to verify it */
+            /* NOTE: this should reload the previously saved binary key */
+            ret = bgpsec_load_key(&bgpconfig, filePrefix, &key_data, curveId, 1);
+            RESULT(("cert sign: loading key function returned: %d (should be %d)",
+                    ret, BGPSEC_SUCCESS), ret == BGPSEC_SUCCESS);
+
+            /* verify that the signature matches again with the loaded key */
+            ret = bgpsec_verify_signature_with_cert(&bgpconfig,
+                                                    data_to_sign,
+                                                    sizeof(data_to_sign),
+                                                    key_data,
+                                                    signature_algorithms[algorithm_count],
+                                                    signature, signature_len);
+            RESULT(("cert sign: verify signature result of generated bin key: %d (should be %d)",
+                    ret, BGPSEC_SIGNATURE_MATCH),
+                   ret == BGPSEC_SIGNATURE_MATCH);
+
+
+            /* Nuke the binary version of the key, and make sure the x.509
+               gets reloaded again */
+            snprintf(fileName, sizeof(fileName), "%s.bin_pub", filePrefix);
+            unlink(fileName);
+        
+            ret = bgpsec_load_key(&bgpconfig, filePrefix, &key_data, curveId, 1);
+            RESULT(("cert sign: loading key function returned: %d (should be %d)",
+                    ret, BGPSEC_SUCCESS), ret == BGPSEC_SUCCESS);
+        
+            /* verify that the signature matches again with the loaded key */
+            ret = bgpsec_verify_signature_with_cert(&bgpconfig,
+                                                    data_to_sign,
+                                                    sizeof(data_to_sign),
+                                                    key_data,
+                                                    signature_algorithms[algorithm_count],
+                                                    signature, signature_len);
+            RESULT(("cert sign: verify signature result of non-bin: %d (should be %d)",
+                    ret, BGPSEC_SIGNATURE_MATCH),
+                   ret == BGPSEC_SIGNATURE_MATCH);
+        
+
+
+            /* completely get rid of the current key */
+            EC_KEY_free(key_data.ecdsa_key);
+            key_data.ecdsa_key = NULL;
+
+            /* now reload just the public part of the key and test just it */
+            ret = bgpsec_load_key(&bgpconfig, filePrefix, &key_data, curveId, 0);
+            RESULT(("cert sign: loading public key function returned: %d (should be %d)",
+                    ret, BGPSEC_SUCCESS), ret == BGPSEC_SUCCESS);
+        
+            /* verify that the signature matches again with the public key */
+            ret = bgpsec_verify_signature_with_cert(&bgpconfig,
+                                                    data_to_sign,
+                                                    sizeof(data_to_sign),
+                                                    key_data,
+                                                    signature_algorithms[algorithm_count],
+                                                    signature, signature_len);
+            RESULT(("cert sign: verify (pub) signature result: %d (should be %d)",
+                    ret, BGPSEC_SIGNATURE_MATCH),
+                   ret == BGPSEC_SIGNATURE_MATCH);
+        
+            /* generate a signature using a fingerprint */
+            /* XXX: set test directory to search for matching ski->certs */
+            signature_len =
+                bgpsec_sign_data_with_ascii_ski(&bgpconfig,
                                                 data_to_sign,
                                                 sizeof(data_to_sign),
-                                                key_data,
+                                                ski, strlen(ski)+1,
                                                 signature_algorithms[algorithm_count],
-                                                signature, signature_len);
-        RESULT(("cert sign: verify signature fail result: %d (should be %d)",
-                ret, BGPSEC_SIGNATURE_MISMATCH),
-               ret == BGPSEC_SIGNATURE_MISMATCH);
-        
-        /* completely get rid of the current key */
-        EC_KEY_free(key_data.ecdsa_key);
-        key_data.ecdsa_key = NULL;
+                                                signature, sizeof(signature));
 
-        /* now reload the key from the files and use them to verify it */
-        ret = bgpsec_load_key(&bgpconfig, filePrefix, &key_data, curveId, 1);
-        RESULT(("cert sign: loading key function returned: %d (should be %d)",
-                ret, BGPSEC_SUCCESS), ret == BGPSEC_SUCCESS);
-        
-        /* verify that the signature matches again with the loaded key */
-        ret = bgpsec_verify_signature_with_cert(&bgpconfig,
-                                                data_to_sign,
-                                                sizeof(data_to_sign),
-                                                key_data,
-                                                signature_algorithms[algorithm_count],
-                                                signature, signature_len);
-        RESULT(("cert sign: verify signature result: %d (should be %d)",
-                ret, BGPSEC_SIGNATURE_MATCH),
-               ret == BGPSEC_SIGNATURE_MATCH);
-
-        /* completely get rid of the current key */
-        EC_KEY_free(key_data.ecdsa_key);
-        key_data.ecdsa_key = NULL;
-
-        /* now reload just the public part of the key and test just it */
-        ret = bgpsec_load_key(&bgpconfig, filePrefix, &key_data, curveId, 0);
-        RESULT(("cert sign: loading public key function returned: %d (should be %d)",
-                ret, BGPSEC_SUCCESS), ret == BGPSEC_SUCCESS);
-        
-        /* verify that the signature matches again with the public key */
-        ret = bgpsec_verify_signature_with_cert(&bgpconfig,
-                                                data_to_sign,
-                                                sizeof(data_to_sign),
-                                                key_data,
-                                                signature_algorithms[algorithm_count],
-                                                signature, signature_len);
-        RESULT(("cert sign: verify (pub) signature result: %d (should be %d)",
-                ret, BGPSEC_SIGNATURE_MATCH),
-               ret == BGPSEC_SIGNATURE_MATCH);
-        
-        /* generate a signature using a fingerprint */
-        /* XXX: set test directory to search for matching ski->certs */
-        signature_len =
-            bgpsec_sign_data_with_ascii_ski(&bgpconfig,
-                                            data_to_sign,
-                                            sizeof(data_to_sign),
-                                            ski, strlen(ski)+1,
-                                            signature_algorithms[algorithm_count],
-                                            signature, sizeof(signature));
-
-        RESULT(("ski sign:   algorithm %d, signature length (%d) is not negative",
-                signature_algorithms[algorithm_count], signature_len),
-               signature_len > -1);
-        RESULT(("ski sign:   algorithm %d, signature length (%d) has at least a byte", signature_algorithms[algorithm_count], signature_len), signature_len > 0);
+            RESULT(("ski sign:   algorithm %d, signature length (%d) is not negative",
+                    signature_algorithms[algorithm_count], signature_len),
+                   signature_len > -1);
+            RESULT(("ski sign:   algorithm %d, signature length (%d) has at least a byte", signature_algorithms[algorithm_count], signature_len), signature_len > 0);
         
 
-        /* verify that the signature matches */
-        ret = bgpsec_verify_signature_with_ascii_ski(&bgpconfig,
-                                                     data_to_sign,
-                                                     sizeof(data_to_sign),
-                                                     ski, strlen(ski)+1,
-                                                     signature_algorithms[algorithm_count],
-                                                     signature, sizeof(signature));
-        RESULT(("ski sign:   verify signature result: %d (should be %d)",
-                ret, BGPSEC_SIGNATURE_MATCH),
-               ret == BGPSEC_SIGNATURE_MATCH);
+            /* verify that the signature matches */
+            ret = bgpsec_verify_signature_with_ascii_ski(&bgpconfig,
+                                                         data_to_sign,
+                                                         sizeof(data_to_sign),
+                                                         ski, strlen(ski)+1,
+                                                         signature_algorithms[algorithm_count],
+                                                         signature, sizeof(signature));
+            RESULT(("ski sign:   verify signature result: %d (should be %d)",
+                    ret, BGPSEC_SIGNATURE_MATCH),
+                   ret == BGPSEC_SIGNATURE_MATCH);
 
-        /* move on to the next algorithm */
-        algorithm_count++;
+            /* move on to the next algorithm */
+            algorithm_count++;
+        }
+
     }
     
     printf("\nResults:\n");
